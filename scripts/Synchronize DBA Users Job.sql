6adbc593354b0493e442bb6169b1f673a4e0e476
Jon Heller <jonearles@yahoo.com>
2017-05-01 22:58:15 -0500

    Prevent one bad account from stopping all processing.

--- scripts/Synchronize DBA Users Job.sql
+++ scripts/Synchronize DBA Users Job.sql
@@ -4,7 +4,7 @@
 --	1. The central management database must be 11g.  12c will require a few minor changes to password hash algorithms.
 --	2. The user running this script must be able to use Method5.
 --	3. The user running the script must have been granted SELECT on SYS.USER$.
---Version: 2.0.0
+--Version: 2.0.1
 
 
 
@@ -56,7 +56,7 @@ create or replace procedure synch_dba_users authid current_user is
 
 	type string_table is table of varchar2(4000);
 	v_users string_table := string_table();
-
+	v_error_message varchar2(32767);
 begin
 	--Check version.  Raise error if management database is 12c or above.
 	--This script does not yet support a 12c management database, but it will work with 12c targets.
@@ -87,111 +87,124 @@ begin
 			]'
 			into v_table_name, v_des_password_hash, v_sha1_password_hash
 			using v_users(i);
-		exception when no_data_found then
-			raise_application_error(-20000, 'The user '||v_users(i)||' was not found.  That user must '||
-				'first exist on '||sys_context('userenv', 'server_host')||' before it can be copied to other servers.');
+		exception when no_data_found then null;
 		end;
 
-		--Throw error if the user does not have a DES hash.
-		--A SHA1 hash is still required for a few databases.
-		if v_des_password_hash is null then
-			raise_application_error(-20000, 'The user '||v_users(i)||' does not have a DES password hash.'||
-				'  Databases with SEC_CASE_SENSITIVE_LOGON=false still require a DES password hash.');
-		end if;
+		--Create error if the user is not found
+		if v_table_name is null then
 
-		--Maintain account.
-		m5_proc(
-			p_table_name			=> v_table_name,
-			p_table_exists_action	=> 'DROP',
-			p_asynchronous			=> false,
-			--Example of how to run on only a subset of databaes.
-			--p_targets 	=> 'pqdwdv01',
-			p_code					=> 
-			replace(replace(replace(q'{
-				--DBA account management.
-				declare
-					v_sec_case_sensitive_logon varchar2(4000);
+			v_error_message := v_error_message || 'The user '||v_users(i)||' was not found.  That user must '||
+				'first exist on '||sys_context('userenv', 'server_host')||' before it can be copied to other servers.' || chr(10);
+		--Create error if the user does not have a DES hash.
+		--A DES hash is still required for some databases.
+		elsif v_des_password_hash is null then
 
-					v_new_password varchar2(4000);
-					v_username varchar2(128) := upper('#USERNAME#');
+			v_error_message := v_error_message || 'The user '||v_users(i)||' does not have a DES password hash.'||
+				'  Databases with SEC_CASE_SENSITIVE_LOGON=false still require a DES password hash.' || chr(10);
+		--Only process this user if there is no error.
+		else
 
-					v_old_password varchar2(4000);
-					v_account_status varchar2(4000);
-					v_profile varchar2(4000);
-					v_has_dba_role number;
-				begin
-					--Find if this database supports case-sensitive passwords.
-					select upper(trim(value))
-					into v_sec_case_sensitive_logon
-					from v$parameter
-					where name = 'sec_case_sensitive_logon';
+			--Maintain account.
+			m5_proc(
+				p_table_name          => v_table_name,
+				p_table_exists_action => 'DROP',
+				p_asynchronous        => false,
+				--Example of how to run on only a subset of databaes.
+				--p_targets 	=> 'pqdwdv01',
+				p_code					=> 
+				replace(replace(replace(q'{
+					--DBA account management.
+					declare
+						v_sec_case_sensitive_logon varchar2(4000);
 
-					--Use SHA1 only if possible
-					if v_sec_case_sensitive_logon = 'TRUE' then
-						v_new_password := '#SPARE4#';
+						v_new_password varchar2(4000);
+						v_username varchar2(128) := upper('#USERNAME#');
 
-						select spare4, account_status, profile
-						into v_old_password, v_account_status, v_profile
-						from sys.user$ join dba_users on sys.user$.name = dba_users.username
-						where user$.name = v_username;
+						v_old_password varchar2(4000);
+						v_account_status varchar2(4000);
+						v_profile varchar2(4000);
+						v_has_dba_role number;
+					begin
+						--Find if this database supports case-sensitive passwords.
+						select upper(trim(value))
+						into v_sec_case_sensitive_logon
+						from v$parameter
+						where name = 'sec_case_sensitive_logon';
 
-					--Else use both.
-					else
-						v_new_password := '#SPARE4#;#DES#';
+						--Use SHA1 only if possible
+						if v_sec_case_sensitive_logon = 'TRUE' then
+							v_new_password := '#SPARE4#';
 
-						select spare4||';'||user$.password, account_status, profile
-						into v_old_password, v_account_status, v_profile
-						from sys.user$ join dba_users on sys.user$.name = dba_users.username
-						where user$.name = v_username;
-					end if;
+							select spare4, account_status, profile
+							into v_old_password, v_account_status, v_profile
+							from sys.user$ join dba_users on sys.user$.name = dba_users.username
+							where user$.name = v_username;
 
-					--Reset password.
-					if nvl(v_old_password, 'NULL') <> v_new_password then
-						declare
-							v_pw_cannot_be_reused exception;
-							pragma exception_init(v_pw_cannot_be_reused, -28007);
-						begin
-							execute immediate 'alter user '||v_username||' identified by values '''||v_new_password||'''';
-							dbms_output.put_line('Password reset.');
-						exception when v_pw_cannot_be_reused then
-							dbms_output.put_line('Password not changed - password cannot be reused.');
-						end;
-					end if;
+						--Else use both.
+						else
+							v_new_password := '#SPARE4#;#DES#';
 
-					--Unlock account.
-					if v_account_status like '%LOCK%' then
-						dbms_output.put_line('Account unlocked.');
-						execute immediate 'alter user '||v_username||' account unlock';
-					end if;
+							select spare4||';'||user$.password, account_status, profile
+							into v_old_password, v_account_status, v_profile
+							from sys.user$ join dba_users on sys.user$.name = dba_users.username
+							where user$.name = v_username;
+						end if;
 
-					--Fix profile.
-					if v_profile <> 'DBA_PROFILE' then
-						dbms_output.put_line('Profile altered.');
-						execute immediate 'alter user '||v_username||' profile dba_profile';
-					end if;
+						--Reset password.
+						if nvl(v_old_password, 'NULL') <> v_new_password then
+							declare
+								v_pw_cannot_be_reused exception;
+								pragma exception_init(v_pw_cannot_be_reused, -28007);
+							begin
+								execute immediate 'alter user '||v_username||' identified by values '''||v_new_password||'''';
+								dbms_output.put_line('Password reset.');
+							exception when v_pw_cannot_be_reused then
+								dbms_output.put_line('Password not changed - password cannot be reused.');
+							end;
+						end if;
 
-					--Grant DBA.
-					select count(*)
-					into v_has_dba_role
-					from dba_role_privs
-					where grantee = v_username
-						and granted_role = 'DBA';
+						--Unlock account.
+						if v_account_status like '%LOCK%' then
+							dbms_output.put_line('Account unlocked.');
+							execute immediate 'alter user '||v_username||' account unlock';
+						end if;
 
-					if v_has_dba_role = 0 then
-						dbms_output.put_line('DBA granted.');
+						--Fix profile.
+						if v_profile <> 'DBA_PROFILE' then
+							dbms_output.put_line('Profile altered.');
+							execute immediate 'alter user '||v_username||' profile dba_profile';
+						end if;
+
+						--Grant DBA.
+						select count(*)
+						into v_has_dba_role
+						from dba_role_privs
+						where grantee = v_username
+							and granted_role = 'DBA';
+
+						if v_has_dba_role = 0 then
+							dbms_output.put_line('DBA granted.');
+							execute immediate 'grant dba to '||v_username;
+						end if;
+
+					--If no account, create one.
+					exception when no_data_found then
+						execute immediate 'create user '||v_username||' identified by values '''||v_new_password||'''';
 						execute immediate 'grant dba to '||v_username;
-					end if;
-
-				--If no account, create one.
-				exception when no_data_found then
-					execute immediate 'create user '||v_username||' identified by values '''||v_new_password||'''';
-					execute immediate 'grant dba to '||v_username;
-					execute immediate 'alter user '||v_username||' profile dba_profile';
-					dbms_output.put_line('Account created.');
-				end;
-			}', '#USERNAME#', v_users(i)), '#DES#', v_des_password_hash), '#SPARE4#', v_sha1_password_hash)
-		);
+						execute immediate 'alter user '||v_username||' profile dba_profile';
+						dbms_output.put_line('Account created.');
+					end;
+				}', '#USERNAME#', v_users(i)), '#DES#', v_des_password_hash), '#SPARE4#', v_sha1_password_hash)
+			);
+		end if;
 	end loop;
+
+	--Raise the error, if any, after the processing is complete.
+	--This way one error won't stop everyone else.
+	if v_error_message is not null then
+		raise_application_error(-20000, v_error_message);
+	end if;
+
 end synch_dba_users;
 /
 
